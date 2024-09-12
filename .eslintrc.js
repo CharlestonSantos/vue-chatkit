2d7b140c01ed393fecd6a0a11607fbff360a98ec
CharlestonSantos <136201230+CharlestonSantos@users.noreply.github.com>
2024-09-12 09:36:15 -0300

    Add Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   }
 }
